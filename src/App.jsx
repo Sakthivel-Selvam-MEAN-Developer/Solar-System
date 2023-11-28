6b30c24eddc963434a5baf004f8f490bc824e185
Sakthivel Selvam <sakthiselvamcs@outlook.com>
2023-11-28 17:10:07 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import {Routes, Route} from 'react-router-dom'
 import { Home } from './components/home/home'
+import { NotFound } from './components/404/404'
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route  path='/' element={<Home />}/>
+        <Route  path='*' element={<Home />}/>
       </Routes>
     </>
   )
---
+++ src/components/404/404.jsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+    return (
+        <>
+            <p className="text-center text-white">Not Found</p>
+        </>
+    )
+}
+
+export { NotFound }
