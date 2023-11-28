861bbf13fb1973fb2125b910954e3830c4fa53ae
Sakthivel Selvam <sakthiselvamcs@outlook.com>
2023-11-28 17:17:59 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 import {Routes, Route} from 'react-router-dom'
 import { Home } from './components/home/home'
-import { NotFound } from './components/404/404'
 
 function App() {
 
--- src/components/404/404.jsx
+++
@@ -1,9 +0,0 @@
-const NotFound = () => {
-    return (
-        <>
-            <p className="text-center text-white">Not Found</p>
-        </>
-    )
-}
-
-export { NotFound }
